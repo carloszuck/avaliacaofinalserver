cfa6b3670e2cb6674ec9802314bf42c5d387c88d
carloszuck <carlosmiguelzpb.cmzpb77@gmail.com>
2020-11-27 19:16:29 -0200

    feat: Add all logic to the UserController

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,23 +1,66 @@
+const User = require('../models/User');
+
 class UserController {
-    create(req, res) {
+    async create(req, res) {
+        const { firstname, lastname, age, email, address } = req.body;
 
-    }
+        try {
+            const createdUser = await User.create({ firstname, lastname, age, email, address });
 
-    delete(req, res) {
+            return res.status(200).json(createdUser);
+        } catch(error) {
+            return res.status(400).json(err);
+        }
         
     }
 
-    index(req, res) {
+    async delete(req, res) {
+        const { _id } = req.params;
+
+        try {
+            const deleted = await User.deleteOne({ _id });
+
+            return res.status(200).json(deleted);
+        } catch (err) {
+            return res.status(400).json(err);
+        }
+    }
+
+    async index(req, res) {
+        try {
+            const users = await User.find();
+
+            return res.status(200).json(users);
+        } catch (err) {
+            return res.status(400).json(err);
+        }
         
     }
 
-    show(req, res) {
+    async show(req, res) {
+        const { _id } = req.params;
+
+        try {
+            const user = await User.findOne({ _id });
+
+            return res.status(200).json(user);
+        } catch (err) {
+            return res.status(400).json(err);
+        }
         
     }
 
-    update(req, res) {
-        
+    async update(req, res) {
+        const { _id } = req.params;
+
+        try {
+            const updated = await User.update({ _id }, req.body).exec();
+
+            return res.status(200).json(updated);
+        } catch (err) {
+            return res.status(400).json(err);
+        }
     }
 }
 
-module.exposrts = new UserController();
+module.exports = new UserController();
